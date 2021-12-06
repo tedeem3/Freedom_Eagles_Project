5769896fe4cdd88373892caad08ba13b1d68dbf4
tedeem3 <38950047+tedeem3@users.noreply.github.com>
2021-12-06 11:10:58 -0500

    Add files via upload

---
+++ Freedom_Project/function.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+// end of nav bar js code
+// Beginning of calendar js code
+
